a3d18c56d0ca6ba6a921fc2559ea8e1ecf39b4a7
antoneous <antonio.redekop@gmail.com>
2025-07-24 12:15:43 -0700

    feat: get basic robot record fields using Jira REST API

--- app/main.py
+++ app/main.py
@@ -9,17 +9,17 @@ load_dotenv()
 JIRA_EMAIL = os.getenv("JIRA_EMAIL") 
 JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN") 
 JIRA_DOMAIN = os.getenv("JIRA_DOMAIN")
+JIRA_URL = f"https://{JIRA_DOMAIN}/rest/api/3/issue/"
 
-def get_jira_issue():
-    issue_key = "POPS-2575"
-
+def get_robot_issue(issue_key):
     auth = HTTPBasicAuth(JIRA_EMAIL, JIRA_API_TOKEN)
-
-    url = f"https://{JIRA_DOMAIN}/rest/api/3/issue/{issue_key}"
+    url = f"{JIRA_URL}{issue_key}"
     headers = {
         "Accept": "application/json"
     }
-
     response = requests.get(url, headers=headers, auth=auth)
-    # print(response.json())
-    return response.status_code
+
+    if response.status_code != 200:
+        raise RuntimeError(f"Failed to fetch issue {issue_key}: {response.status_code} - {response.text}")
+    # return the response as a Python dict
+    return response.json()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,19 @@
-from app.main import get_jira_issue
+from app.main import get_robot_issue
 
-def test_hello():
-    assert get_jira_issue() == 200
+def test_get_robot_issue():
+    try:
+        robot_issue = get_robot_issue("POPS-2575");  # <--- known good issue key
+        #robot_issue = get_robot_issue("POPS-9999"); # <--- non-existent issue key
+    except RuntimeError as e:
+        print(f"Failed to get robot issue: {e}")
+        raise
+
+    fields = robot_issue["fields"]
+    assignee = fields["assignee"]["displayName"] if fields["assignee"] else "Unassigned"
+    description = fields["description"]["content"][0]["content"][0]["text"]
+
+    assert description == "Here is the description"
+    assert assignee == "Antonio Redekop"
+    assert fields["summary"] == "Jaeger Test Ticket - Do Not Use"
+    assert fields["status"]["name"] == "In Progress"
+    assert fields["created"] == "2025-07-22T16:44:08.222-0700"
