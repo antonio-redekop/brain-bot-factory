96365b6f442e6d2e7553da5ff8b405ebb3c9aab4
antoneous <antonio.redekop@gmail.com>
2025-07-24 14:24:48 -0700

    feat: get comment field using Jira REST API

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,9 +11,14 @@ def test_get_robot_issue():
     fields = robot_issue["fields"]
     assignee = fields["assignee"]["displayName"] if fields["assignee"] else "Unassigned"
     description = fields["description"]["content"][0]["content"][0]["text"]
+    # extract a list of comments
+    comments = [
+        c["body"]["content"][0]["content"][0]["text"] for c in fields["comment"]["comments"]
+    ]
 
     assert description == "Here is the description"
     assert assignee == "Antonio Redekop"
     assert fields["summary"] == "Jaeger Test Ticket - Do Not Use"
     assert fields["status"]["name"] == "In Progress"
     assert fields["created"] == "2025-07-22T16:44:08.222-0700"
+    assert comments[0] == "Test Comment #1"
